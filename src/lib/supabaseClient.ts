8b4cd623f441370d79f8e11501f01cc0ec424d09
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-11-14 10:11:39 -0500

    update

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,3 @@
-// Este archivo debe ser renombrado manualmente a supabaseClient.ts para corregir el error de mayúsculas/minúsculas.
-// Archivo renombrado a supabaseClient.ts para corregir el error de mayúsculas/minúsculas
 declare global {
   interface ImportMetaEnv {
     readonly VITE_SUPABASE_KEY: string;
